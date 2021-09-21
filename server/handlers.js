b7776a1e8bb561d964afbeca3621a8307725bbe8
Anissah Abbas <anissah.abbas@mail.mcgill.ca>
2021-09-21 13:05:57 -0400

    anissah first three endpoints

--- server/handlers.js
+++ server/handlers.js
@@ -2,69 +2,83 @@
 const { v4: uuidv4 } = require("uuid");
 
 //Import data
-const items = require("./data/fixedItems.json");
-const companies = require("./data/fixedCompanies.json");
+const items = require("./data/items.json");
+const companies = require("./data/companies.json");
 //Impory helper functions
 const { sendResponse } = require("./utills");
 
 // //Mongodb
 // //Connect to the DB
-// const { MongoClient } = require("mongodb");
-// require("dotenv").config();
-// const { MONGO_URI } = process.env;
-// const assert = require("assert");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const assert = require("assert");
 
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ };
+
 
 const getAllProducts = async (req, res) => {
-  try {
-    sendResponse({ res, status: 200, data: items });
-  } catch (err) {
-    sendResponse({ res, status: 400, message: err.message });
-  }
-};
-
-
-const getCompanyById = (req, res) => {
-    const id = Number(req.params._id);
-    const foundCompany = companies.find((company) => company._id === id);
-    if (foundCompany) {
-        res.status(200).json({
-            status: 200,
-            message: 'Success: Company Found!',
-            data: foundCompany
-        });
-    } else {
-        res.status(400).json({
-            status: 400,
-            message: 'No company with that ID found',
-            dataSent: req.params
-        });
+    try {
+        const client = new MongoClient(MONGO_URI, options);
+        await client.connect();
+        const db = client
+        sendResponse({ res, status: 200, data: items });
+    } catch (err) {
+        sendResponse({ res, status: 400, message: err.message });
     };
 };
-/* const updateInventory = (req, res) => {
+
+const getProductById = (req, res) => {
     try {
-        let updatedItem = '';
+        const id = Number(req.params._id);
+        const foundItem = items.find((item) => item._id === id);
+        if (foundItem) {
+            sendResponse({ res, status: 200, data: foundItem })
+        } else {
+            sendResponse({ res, status: 400, message: 'Error: cannot find item', data: req.params });
+        };
+    } catch (err) {
+        console.log(err.stack);
+        sendResponse({ res, status: 500, message: err.message, data: req.params });
+    };
+};
+
+const getCompanyById = (req, res) => {
+    try {
+        const id = Number(req.params._id);
+        const foundCompany = companies.find((company) => company._id === id);
+        if (foundCompany) {
+            sendResponse({ res, status: 200, data: foundCompany });
+        } else {
+            sendResponse({ res, status: 400, data: req.params, message: 'Error: Company not found' });
+        };
+    } catch (err) {
+        console.log(err.stack);
+        sendResponse({ res, status: 500, message: err.message, data: req.params })
+    };
+};
+
+const updateInventory = (req, res) => {
+   /*  try {
         const itemId = Number(req.params_id);
         const numPurchased = Number(req.body.numPurchased);
         for (let item of items) {
             if (item._id === itemId) {
-                item.numInStock =- numPurchased;
-                updatedItem = item;
-            }
+                item.numInStock -= numPurchased;
+                console.log(item.numInStock, 'hit');
         }
         res.status(200).json({
             status: 200,
             message: "Item updated!",
-            data: updatedItem
+            data: 'ok'
         })
+    }
     } catch (err) {
         console.log(err.stack)
-    }
+    } */
+};
 
-} */
-
-module.exports = { getAllProducts, getCompanyById, updateInventory };
+module.exports = { getAllProducts, getProductById, getCompanyById, updateInventory };
