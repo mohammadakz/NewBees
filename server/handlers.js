4d2feb0482b3efcddf5b2aef8b6badcbafbf1021
Anissah Abbas <anissah.abbas@mail.mcgill.ca>
2021-09-21 15:18:46 -0400

    all endpoints working w mongodb anissah

--- server/handlers.js
+++ server/handlers.js
@@ -2,8 +2,8 @@
 const { v4: uuidv4 } = require("uuid");
 
 //Import data
-const items = require("./data/items.json");
-const companies = require("./data/companies.json");
+//const items = require("./data/items.json");
+//const companies = require("./data/companies.json");
 //Impory helper functions
 const { sendResponse } = require("./utills");
 
@@ -13,72 +13,91 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 const assert = require("assert");
+const e = require("express");
 
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
- };
+};
 
+const dbName = 'E-commerce';
 
 const getAllProducts = async (req, res) => {
     try {
         const client = new MongoClient(MONGO_URI, options);
         await client.connect();
-        const db = client
-        sendResponse({ res, status: 200, data: items });
+        const db = client.db(dbName);
+        const products = await db.collection('Products').find().toArray();
+        client.close();
+        sendResponse({ res, status: 200, data: products });
     } catch (err) {
         sendResponse({ res, status: 400, message: err.message });
     };
 };
 
-const getProductById = (req, res) => {
+const getProductById = async (req, res) => {
     try {
-        const id = Number(req.params._id);
-        const foundItem = items.find((item) => item._id === id);
-        if (foundItem) {
-            sendResponse({ res, status: 200, data: foundItem })
-        } else {
-            sendResponse({ res, status: 400, message: 'Error: cannot find item', data: req.params });
-        };
+        const _id = Number(req.params._id);
+        const client = new MongoClient(MONGO_URI, options);
+        await client.connect();
+        const db = client.db(dbName);
+        db.collection('Products').findOne({ _id }, (err, result) => {
+            result
+                ? sendResponse({ res, status: 200, data: result })
+                : sendResponse({ res, status: 400, message: 'Error: cannot find item', data: req.params });
+            client.close();
+        })
     } catch (err) {
         console.log(err.stack);
         sendResponse({ res, status: 500, message: err.message, data: req.params });
     };
 };
 
-const getCompanyById = (req, res) => {
+const getCompanyById = async (req, res) => {
     try {
-        const id = Number(req.params._id);
-        const foundCompany = companies.find((company) => company._id === id);
-        if (foundCompany) {
-            sendResponse({ res, status: 200, data: foundCompany });
-        } else {
-            sendResponse({ res, status: 400, data: req.params, message: 'Error: Company not found' });
-        };
+        const _id = Number(req.params._id);
+        const client = new MongoClient(MONGO_URI, options);
+        await client.connect();
+        const db = client.db(dbName);
+        db.collection('Companies').findOne({ _id }, (err, result) => {
+            result
+                ? sendResponse({ res, status: 200, data: result })
+                : sendResponse({ res, status: 400, message: 'Error: Company not found', data: req.params });
+            client.close();
+        })
     } catch (err) {
         console.log(err.stack);
         sendResponse({ res, status: 500, message: err.message, data: req.params })
     };
 };
 
-const updateInventory = (req, res) => {
-   /*  try {
-        const itemId = Number(req.params_id);
+const updateInventory = async (req, res) => {
+    try {
+        const _id = Number(req.params._id);
         const numPurchased = Number(req.body.numPurchased);
-        for (let item of items) {
-            if (item._id === itemId) {
-                item.numInStock -= numPurchased;
-                console.log(item.numInStock, 'hit');
-        }
-        res.status(200).json({
-            status: 200,
-            message: "Item updated!",
-            data: 'ok'
-        })
-    }
+        const client = new MongoClient(MONGO_URI, options);
+        await client.connect();
+        const db = client.db(dbName);
+        db.collection('Products').findOne({ _id }, async (err, result) => {
+            if (result.numInStock - numPurchased < 0) {
+                sendResponse({
+                    res,
+                    status: 400,
+                    message: 'Error not enough stock',
+                    stockLeft: result.numInStock,
+                    amountRequested: numPurchased
+                })
+            } else {
+                const query = { _id };
+                const newValues = { $set: { numInStock: result.numInStock - numPurchased } };
+                await db.collection('Products').updateOne(query, newValues);
+                client.close();
+                sendResponse({ res, status: 200, ...newValues.$set, numPurchased })
+            }
+        });
     } catch (err) {
         console.log(err.stack)
-    } */
+    }
 };
 
 module.exports = { getAllProducts, getProductById, getCompanyById, updateInventory };
