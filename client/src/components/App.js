31f1a0786752ea5a2020242f5445f82287a44f40
maxgaillard <maxgaillard@hotmail.com>
2021-09-21 10:59:06 -0400

    general components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Header from "./Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import ProductDetails from "./ProductDetails";
 
 function App() {
-  const [bacon, setBacon] = useState(null);
-
-  useEffect(() => {
-    fetch('/bacon')
-      .then(res => res.json())
-      .then(data => setBacon(data));
-  }, []);
-
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  return (
+    <Router>
+      <Header>
+        <Nav />
+      </Header>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/contact">
+          <About />
+        </Route>
+        <Route path="/about">
+          <Contact />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetails />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/ProductDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProductDetails = () => {
+  return <div></div>;
+};
+
+export default ProductDetails;
